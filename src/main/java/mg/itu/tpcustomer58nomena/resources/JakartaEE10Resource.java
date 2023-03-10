ae9656322618837deda1badbef7178ad5c537256
Nomena <razakamalala.nomena@gmail.com>
2023-03-10 12:33:25 +0300

    Etat initial

---
+++ src/main/java/mg/itu/tpcustomer58nomena/resources/JakartaEE10Resource.java
@@ -0,0 +1,19 @@
+package mg.itu.tpcustomer58nomena.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
